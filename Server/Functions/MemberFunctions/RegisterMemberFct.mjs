265f5497ba82a8465dc55492388cad1c85caf086
Sassi Mohamed Amine <sssmouhamedamine@gmail.com>
2025-08-17 16:19:46 +0100

    first Commit in fs

--- Server/Functions/MemberFunctions/RegisterMemberFct.mjs
+++ Server/Functions/MemberFunctions/RegisterMemberFct.mjs
@@ -1,11 +1,25 @@
 import moment from "moment/moment.js";
 import { Member } from "../../DB_Schema/memberSchema.mjs";
-const registerMember = (req, res) => {
+import { promises as fsPromises } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const registerMember = async (req, res) => {
   const { firstName, lastName, phoneNumber, dateOfBirth, specialty } = req.body;
 
   const subscriptionDay = moment().format("MM-DD-YYYY");
   const endSubscriptionDay = moment().add(1, "month").format("MM-DD-YYYY");
 
+  await fsPromises.appendFile(
+    path.join(__dirname, "..", "..", "..", "..", "Members.txt"),
+    `${firstName}-${lastName}\t/ ${phoneNumber}\t /${dateOfBirth}\t /${specialty}\t /${subscriptionDay}->${endSubscriptionDay}\n`,
+    (err) => {
+      console.log("Error has accured:", err);
+    }
+  );
   const newMember = new Member({
     firstName,
     lastName,
